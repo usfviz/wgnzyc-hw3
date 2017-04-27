75f124e4feb70b46e3a5d59a5fc9845a77af3d7d
wgnzyc <yzhu60@dons.usfca.edu>
2017-04-26 20:03:21 -0700

    app

---
+++ app.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(ggvis)
+library(reshape2)
+library(plotly)
+library(GGally)
+
+cat('\014')
+setwd('~/workspace/MSAN_viz/hw3/')
+fb <- read.csv('dataset_Facebook.csv',sep = ';')
+types <- levels(fb$Type)
+y_axis <- c("Lifetime.Post.Total.Reach","Lifetime.Post.Total.Impressions","Lifetime.Engaged.Users","Lifetime.Post.Consumers","Lifetime.Post.Consumptions")
+x_axis <- c("Post.Month","Post.Weekday")
+
+ui <- fluidPage(
+  headerPanel('Facebook data'),
+  sidebarPanel(
+    radioButtons("type", "Type", choices = types)
+    
+  ),
+  mainPanel(
+    uiOutput("ggvis_ui"),
+    ggvisOutput("ggvis"),
+    tabsetPanel(id = 'tabs',
+      tabPanel("user engaged", plotlyOutput("hist1"),value = 1),
+      tabPanel("scatterplot matrix", plotlyOutput("hist2"), value = 2) ,
+      tabPanel("Parallel Coordinates Plot", plotlyOutput("hist3"),value = 3))
+  )
+)
+
+
+server <- function(input, output) {
+  select_type <- reactive({fb[fb$Type==input$type,]})
+  output$hist1 <- renderPlotly({
+    plot_ly(data = select_type(),x = ~share,y = ~like,type = 'scatter',mode = 'markers',size = ~Lifetime.Engaged.Users,
+            text = ~paste('Users: ', Lifetime.Engaged.Users))
+  })
+  output$hist2 <- renderPlotly({
+    df_tmp <- subset(fb,select = c("Lifetime.Post.Total.Impressions","Lifetime.Engaged.Users","Lifetime.Post.Consumers","Lifetime.Post.Consumptions"))
+    colnames(df_tmp) <- c("Impressions","Users","Consumers","Consumptions")
+    ggplotly(ggpairs(df_tmp))
+  })
+  output$hist3 <- renderPlotly({
+    df_par <- fb[c("Category","Post.Month", "Post.Weekday",
+                 "Post.Hour", 
+                 "Type")]
+    ggplotly(ggparcoord(df_par, 1:4, groupColumn= "Type"))
+    
+  })
+ 
+}
+
+
+shinyApp(ui = ui, server = server)
