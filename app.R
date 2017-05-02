6eb993958997f6850883a409228f0e11aa9b9cb6
wgnzyc <yzhu60@dons.usfca.edu>
2017-05-02 15:06:08 -0700

    update app

--- app.R
+++ app.R
@@ -15,8 +15,9 @@ ui <- fluidPage(
   headerPanel('Facebook data'),
   sidebarPanel(
     conditionalPanel(condition = "input.conditionedPanels == 1",
-    radioButtons("type", "Type", choices = types))
-  ),
+        radioButtons("type", "Type", choices = types))
+    )
+  ,
   mainPanel(
     tabsetPanel(id = 'conditionedPanels',
       tabPanel("user engaged", plotlyOutput("hist1"),value = 1),
@@ -30,20 +31,31 @@ server <- function(input, output) {
   select_type <- reactive({fb[fb$Type==input$type,]})
   output$hist1 <- renderPlotly({
     plot_ly(data = select_type(),x = ~share,y = ~like,type = 'scatter',mode = 'markers',size = ~Lifetime.Engaged.Users,
-            text = ~paste('Users: ', Lifetime.Engaged.Users))
+            hoverinfo = 'text',
+            text = ~paste('Number of like: ', like,
+                          '</br> Number of share: ', share,
+                          '</br> Number of Users: ', Lifetime.Engaged.Users,
+                          '</br> Total Interactions ',Total.Interactions))
   })
   output$hist2 <- renderPlotly({
+    shinyjs::removeClass( class = "sidebar-collapse")
     df_tmp <- subset(fb,select = c("Lifetime.Post.Total.Impressions","Lifetime.Engaged.Users","Lifetime.Post.Consumers","Lifetime.Post.Consumptions"))
     colnames(df_tmp) <- c("Impressions","Users","Consumers","Consumptions")
-    ggplotly(ggpairs(df_tmp))
+    ggplotly(ggpairs(df_tmp,lower = list(
+      continuous = wrap("points",alpha = 0.7, color = "blue",size = 1) 
+    )))
   })
   output$hist3 <- renderPlotly({
     df_par <- fb[c("Category","Post.Month", "Post.Weekday",
                  "Post.Hour", 
                  "Type")]
+    colnames(df_par) <- c("Category","Post Month", "Post Weekday",
+                          "Post Hour", 
+                          "Type")
     ggplotly(ggparcoord(df_par, 1:4, groupColumn= "Type"))
     
   })
+
  
 }
 
